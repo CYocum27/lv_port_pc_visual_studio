adc4701947dca8df5198d54185fbac65dc202c48
CYocum27 <cjyocum27@chaminet.org>
2023-07-20 22:51:47 -0700

    auton selector works :)

--- LVGL.Simulator/VEXLVGL/images.h
+++ LVGL.Simulator/VEXLVGL/images.h
@@ -4,3 +4,4 @@
 #include <stdint.h>
 extern const uint8_t LogoScreen_map[];
 extern const lv_img_dsc_t LogoScreen;
+extern const lv_img_dsc_t background;
--- LVGL.Simulator/VEXLVGL/screen.cpp
+++ LVGL.Simulator/VEXLVGL/screen.cpp
@@ -11,6 +11,7 @@ void initscreen() {
     lv_img_set_src(img, &LogoScreen);
 }
 lv_obj_t* AutonTitle;
+static lv_style_t AutonStyle;
 static void AutonHandler(lv_event_t* e)
 {
     lv_event_code_t code = lv_event_get_code(e);
@@ -25,14 +26,26 @@ static void AutonHandler(lv_event_t* e)
 }
 
 
+
 void autonSelector() {
+
+    lv_style_init(&AutonStyle);
+    lv_color32_t autonText = LV_COLOR_MAKE(255, 255, 255);
+    lv_style_set_text_color(&AutonStyle, autonText);
+    lv_style_set_bg_opa(&AutonStyle, LV_OPA_TRANSP);
+    lv_style_set_border_width(&AutonStyle, 0);
+    lv_style_set_outline_width(&AutonStyle, 0);
+    lv_obj_t* bg = lv_img_create(lv_scr_act());
+    lv_img_set_src(bg, &background);
     AutonTitle = lv_label_create(lv_scr_act());
     lv_label_set_text(AutonTitle, "No Auton Selected");
-    lv_obj_align(AutonTitle, LV_ALIGN_TOP_MID, 0, 0);
+    lv_obj_add_style(AutonTitle, &AutonStyle, 0);
+    lv_obj_align(AutonTitle, LV_ALIGN_TOP_MID, 0, 10);
     lv_obj_t* AutonButtons = lv_btnmatrix_create(lv_scr_act());
     lv_btnmatrix_set_map(AutonButtons, btn_map);
     lv_btnmatrix_set_btn_ctrl_all(AutonButtons, LV_BTNMATRIX_CTRL_CHECKABLE);
     lv_btnmatrix_set_one_checked(AutonButtons, true);
     lv_obj_add_event_cb(AutonButtons, AutonHandler, LV_EVENT_ALL, NULL);
     lv_obj_align(AutonButtons, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_add_style(AutonButtons, &AutonStyle, LV_STATE_FOCUS_KEY);
 }
